fb5825224fbf309dbddb9059258406942294a298
lior <liorsh@shenkar.ac.il>
2018-05-13 13:47:54 +0300

    room second

--- app/src/main/java/com/example/shenkar/androidstudiocourseapp/BirthDayInstanse.java
+++ app/src/main/java/com/example/shenkar/androidstudiocourseapp/BirthDayInstanse.java
@@ -1,6 +1,61 @@
 package com.example.shenkar.androidstudiocourseapp;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+import java.util.Date;
+
+
+@Entity
 public class BirthDayInstanse {
 
+    @PrimaryKey(autoGenerate = true)
+    private  int id;
 
+    @ColumnInfo(name = "first_name")
+    private String firstName;
+    @ColumnInfo(name = "last_name")
+    private String lastName;
+    @ColumnInfo(name = "date")
+    private Date date;
+
+
+    public BirthDayInstanse(String firstName, String lastName, Date date) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
