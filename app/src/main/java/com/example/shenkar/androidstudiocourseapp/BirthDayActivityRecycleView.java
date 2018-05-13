43e96bb821e95d4a05ca3c2e232208e6c2c45b1f
lior <liorsh@shenkar.ac.il>
2018-05-13 13:39:30 +0300

    add room commit

--- app/src/main/java/com/example/shenkar/androidstudiocourseapp/BirthDayActivityRecycleView.java
+++ app/src/main/java/com/example/shenkar/androidstudiocourseapp/BirthDayActivityRecycleView.java
@@ -3,9 +3,11 @@ package com.example.shenkar.androidstudiocourseapp;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -15,32 +17,45 @@ class BirthDayActivityRecycleView extends RecyclerView.Adapter<BirthDayActivityR
     ArrayList<String> birthDay;
 
     public BirthDayActivityRecycleView(Context context, ArrayList<String> birthDay) {
+        Log.d(AppSettings.TAG , "BirthDayActivityRecycleView: BirthDayActivityRecycleView");
         this.context = context;
         this.birthDay = birthDay;
     }
 
     @NonNull
     @Override
-    public BirthDayActivityRecycleView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d(AppSettings.TAG , "onCreateViewHolder: BirthDayActivityRecycleView");
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_birtday_list , parent , false);
-        return new ViewHolder(view);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
     }
 
     @Override
 
     public void onBindViewHolder(@NonNull BirthDayActivityRecycleView.ViewHolder holder, int position) {
-
+            holder.firstName.setText(birthDay.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return birthDay.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        TextView firstName;
+        TextView lastName;
+        TextView myDate;
+
         public ViewHolder(View itemView) {
             super(itemView);
+            Log.d(AppSettings.TAG , "ViewHolder: BirthDayActivityRecycleView");
+            firstName = itemView.findViewById(R.id.activity_birthday_list_first_name);
+            lastName = itemView.findViewById(R.id.activity_birthday_list_last_name_name);
+            myDate = itemView.findViewById(R.id.activity_birthday_list_date);
         }
+
+
     }
 }
--- app/src/main/java/com/example/shenkar/androidstudiocourseapp/BirthdayActivity.java
+++ app/src/main/java/com/example/shenkar/androidstudiocourseapp/BirthdayActivity.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+
+import java.util.ArrayList;
 
 public class BirthdayActivity extends AppCompatActivity {
 
@@ -16,6 +19,7 @@ public class BirthdayActivity extends AppCompatActivity {
     FloatingActionButton fab;
     RecyclerView recyclerView;
     RecyclerView.Adapter adapter;
+    ArrayList<String> test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +29,25 @@ public class BirthdayActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        test = new ArrayList<>();
+        test.add("test1");
+        test.add("test2");
+        test.add("test3");
+        test.add("test4");
+        test.add("test5");
+        test.add("test6");
+        test.add("test7");
+        test.add("test8");
+        test.add("test9");
+        test.add("test10");
+        test.add("test11");
+        test.add("test12");
+        test.add("test13");
+
         recyclerView = findViewById(R.id.birthday_activity_recycler_view);
-        adapter = new BirthDayActivityRecycleView();
+        adapter = new BirthDayActivityRecycleView(this , test );
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
