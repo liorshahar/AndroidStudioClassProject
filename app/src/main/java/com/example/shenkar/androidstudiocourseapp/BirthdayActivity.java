cc41fa61862bc56fa990469868242e66ac5c7ab9
lior <liorsh@shenkar.ac.il>
2018-05-13 11:11:45 +0300

    birthday commit

--- app/src/main/java/com/example/shenkar/androidstudiocourseapp/BirthdayActivity.java
+++ app/src/main/java/com/example/shenkar/androidstudiocourseapp/BirthdayActivity.java
@@ -1,13 +1,33 @@
 package com.example.shenkar.androidstudiocourseapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 
 public class BirthdayActivity extends AppCompatActivity {
+
+
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(AppSettings.TAG , "onCreate: BirthdayActivity");
         setContentView(R.layout.activity_birthday);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
 }
